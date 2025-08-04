656631777e9c9eaa2d4185b909a4a737ca5cd0f9
witwicki <witwicki@gmail.com>
2025-08-03 20:25:46 -0700

    chore: Proper main() method in face.start for ease of serving when imported.

--- pylips/face/start.py
+++ pylips/face/start.py
@@ -22,7 +22,9 @@ def handle_message(message):
     print(f"received to {message['name']}: {message['action_type']}")
     emit('face_control', message, broadcast=True)
 
-if __name__ == '__main__':
+def main()
     #TODO: add argparse for host and port
     socketio.run(app, host='0.0.0.0', port=8000)
-    
+
+if __name__ == '__main__':
+    main()    
